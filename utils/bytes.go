5bf316b505b37bee5962a2bea76b14e45f88c3a5
yixin <yixinin@outlook.com>
2020-03-23 22:22:01 +0800

    update

--- utils/bytes.go
+++ utils/bytes.go
@@ -8,7 +8,7 @@ func BytesToInt64(buf []byte) int64 {
 	return int64(binary.LittleEndian.Uint64(buf))
 }
 func BytesToInt32(buf []byte) int32 {
-	return int32(binary.LittleEndian.Uint64(buf))
+	return int32(binary.LittleEndian.Uint32(buf))
 }
 
 func Int64ToBytes(i int64) []byte {
